79db6a14240793d81158e0058ef5c58bb9203882
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-05-22 12:06:14 +0100

    UIComponent active/inactive work

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -8,12 +8,15 @@ ThisBuild / scalaVersion := "3.4.1"
 ThisBuild / scalafmtOnCompile := true
 ThisBuild / versionScheme := Some("early-semver")
 
-val LogicFirstVersion = "0.3.0"
+val LogicFirstVersion = "0.3.19-SNAPSHOT"
 val githubResolver = "GitHub Package Registry" at "https://maven.pkg.github.com/kindservices/logic-first"
 ThisBuild / resolvers += githubResolver
 
 ThisBuild / semanticdbEnabled := true
 ThisBuild / semanticdbVersion := scalafixSemanticdb.revision
+
+addCommandAlias("removeUnusedImports", ";scalafix RemoveUnused")
+addCommandAlias("organiseImports", ";scalafix OrganizeImports")
 
 // Common settings
 lazy val commonSettings = Seq(
--- src/main/g8/js/src/main/scala/$pckg__packaged$/js/Main.scala
+++ src/main/g8/js/src/main/scala/$pckg__packaged$/js/Main.scala
@@ -2,85 +2,121 @@ package $pckg;format="lower,package"$
 
 import org.scalajs.dom
 import scalatags.JsDom.all.*
-import org.scalajs.dom.{HTMLElement, Node, document, html}
 
-import scala.scalajs.js.Dynamic.global
-import scala.concurrent.Future
-import scala.concurrent.duration.given
-import scala.scalajs.js.JSON
-import scala.scalajs.js.annotation.JSExportTopLevel
 import upickle.default.*
+import kind.logic.js.mermaid._
+import kind.logic.js.goldenlayout._
 
+import org.scalajs.dom.HTMLDivElement
+import org.scalajs.dom.HTMLElement
 import kind.logic.*
 import kind.logic.js.*
 import kind.logic.telemetry.*
+import kind.logic.js.scenarios.*
 import kind.logic.js.svg.*
-
+import scala.scalajs.js.Dynamic.global
 import scala.util.control.NonFatal
-import org.scalajs.dom.HTMLDivElement
 
+object MainPage {
 
+  lazy val svgContainer    = initSvg()
+  lazy val mermaidPage     = initMermaid()
+  lazy val scenarioBuilder = ScenarioBuilder()
+
+  // init SVG
+  private def initSvg() = {
+    val appSvgContainer = div().render
+    EventBus.activeTestScenario.subscribe { scenario =>
+      def fallback = div(s"We couldn't parse the scenario as a DraftContract or Restaurant").render
+      pizzaAsSvg(scenario).getOrElse(fallback)
+
+      appSvgContainer.innerHTML = ""
+      appSvgContainer.appendChild(pizzaAsSvg(scenario).getOrElse(fallback))
+
+    }
+    appSvgContainer
+  }
+
+  private def initMermaid() = {
+    val appMermaidPage = MermaidPage()
+
+    LocalState.addDefaultScenario(
+      TestScenario(
+        "Pizza Happy Path",
+        "",
+        MakePizzaRequest(1, List("cheese", "tomato sauce", "pepperoni")).toJson
+      )
+    )
+
+    EventBus.activeTestScenario.subscribe { scenario =>
+      try {
+        val request = read[MakePizzaRequest](scenario.input)
+
+        val mermaidMarkdown = PizzaOps.asMermaid(request.quantity, request.toppings)
+
+        appMermaidPage.update(scenario, mermaidMarkdown)
+      } catch {
+        case NonFatal(e) =>
+          appMermaidPage.updateError(
+            scenario,
+            s"We couldn't parse the scenario as a MakePizzaRequest: \$e"
+          )
+      }
+    }
+
+    appMermaidPage
+  }
+
+  def pizzaAsSvg(scenario: TestScenario): Option[HTMLDivElement] = {
+    try {
+      val request                = read[MakePizzaRequest](scenario.input)
+      given telemetry: Telemetry = Telemetry()
+      PizzaOps.defaultProgram.orderPizza(request.quantity, request.toppings).execOrThrow()
+      val calls = telemetry.calls.execOrThrow()
+      Option(SvgForCalls(calls))
+    } catch {
+      case NonFatal(e) =>
+        println(s"Error creating svg: \$e")
+        None
+    }
+  }
+
+}
 case class MakePizzaRequest(quantity: Int, toppings: List[String]) derives ReadWriter {
   def toJson = writeJs(this)
 }
 
-def initMermaid() = {
-  LocalState.addDefaultScenario(
-    TestScenario(
-      "Pizza Happy Path",
-      "",
-      MakePizzaRequest(1, List("cheese", "tomato sauce", "pepperoni")).toJson
-    )
-  )
+@scala.scalajs.js.annotation.JSExportTopLevel("initLayout")
+def initLayout(myLayout: GoldenLayout) = {
 
-  EventBus.activeTestScenario.subscribe { scenario =>
-    try {
-      val request = read[MakePizzaRequest](scenario.input)
+  val drawer = HtmlUtils.\$[HTMLElement]("drawer")
 
-      val mermaidMarkdown = PizzaOps.asMermaid(request.quantity, request.toppings)
-
-      val cleaned = mermaidMarkdown
-        .replace("```mermaid", "")
-        .replace("```", "")
-        .trim
-
-      AppSkeleton.mermaidPage.update(scenario, cleaned)
-    } catch {
-      case NonFatal(e) =>
-        AppSkeleton.mermaidPage.updateError(scenario, s"We couldn't parse the scenario as a MakePizzaRequest: \$e")
-    }
+  myLayout.addMenuItem(drawer, "Scenario Builder") { state =>
+    MainPage.scenarioBuilder.content
   }
-}
 
-
-def pizzaAsSvg(scenario: TestScenario): Option[HTMLDivElement] = {
-  try {
-    val request                = read[MakePizzaRequest](scenario.input)
-    given telemetry: Telemetry = Telemetry()
-    val result =
-      PizzaOps.defaultProgram.orderPizza(request.quantity, request.toppings).execOrThrow()
-    val calls = telemetry.calls.execOrThrow()
-    Option(SvgForCalls(calls))
-  } catch {
-    case NonFatal(e) =>
-      println(s"Error creating svg: \$e")
-      None
+  myLayout.addMenuItem(drawer, "Diagram") { state =>
+    MainPage.mermaidPage.element
   }
-}
 
-def initSvg() = {
-  EventBus.activeTestScenario.subscribe { scenario =>
-    def fallback  = div(s"We couldn't parse the scenario as a DraftContract or Restaurant").render
-    val component = pizzaAsSvg(scenario).getOrElse(fallback)
-    AppSkeleton.svgPage.innerHTML = ""
-    AppSkeleton.svgPage.appendChild(component)
+  myLayout.addMenuItem(drawer, "SVG") { state =>
+    MainPage.svgContainer
   }
-}
 
+  //
+  EventBus.activeTabs.subscribe { activeTabs =>
+    // if the UIComponent isn't shown, then we should remove the menu item
+    UIComponent.inactiveComoponents().foreach(_.showMenuItem())
+    activeTabs.foreach(_.hideMenuItem())
+  }
+
+  myLayout.init()
+}
 
 @main
 def mainJSApp(): Unit = {
-
-
-
+  global.window.initLayout = initLayout
+  global.window.createNewComponent = createNewComponent
+  global.window.onComponentCreated = onComponentCreated
+  global.window.onComponentDestroyed = onComponentDestroyed
 }

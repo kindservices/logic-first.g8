c3e7dc1788f1510ee8601289e14ab4120e3f932d
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-10-01 22:14:00 +0100

    Taught Makefile how to build

--- src/main/g8/jvm/src/main/scala/Main.scala
+++ src/main/g8/jvm/src/main/scala/Main.scala
@@ -17,11 +17,11 @@ object Main {
     // a basic mermaid sequence diagram
     val sequence = "diagrams/sequence.md".asPath
     sequence.text = mermaidDiagram
-    println(s"Created ${sequence}")
+    println(s"Created \${sequence}")
 
     // for C4 diagrams
     val c4File = "diagrams/workspace.dsl".asPath
     c4File.text = c4
-    println(s"Created ${c4File}")
+    println(s"Created \${c4File}")
   }
 }
